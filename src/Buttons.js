7b01e0b3b7e571b81aa3bce7ce77206c9c05fa37
akademia108 <kursy@akademia108.pl>
2020-10-09 10:27:39 +0200

    init

---
+++ src/Buttons.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+const Buttons = (props) => {
+    return (
+        <>
+            <button className='btn' onClick={props.changeColor}>
+                red
+            </button>
+            <button className='btn' onClick={props.changeColor}>
+                blue
+            </button>
+            <button className='btn' onClick={props.changeColor}>
+                green
+            </button>
+            <button onClick={()=>props.changeText('Nowy')}>Tekst 1</button>
+            <button onClick={()=>props.changeText('Nowszy')}>Tekst 2</button>
+            <button onClick={()=>props.changeText('Najnowszy')}>Tekst 3</button>
+         </>
+    )
+}
+
+export default Buttons;
